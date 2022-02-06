e71b58a7bfce36c0dda34fd26547704eb0889420
Jacob Prud'homme <jacobprudhomme@users.noreply.github.com>
2022-02-06 12:25:31 -0500

    Separated out function from page

---
+++ lib/posts.ts
@@ -0,0 +1,18 @@
+import PostT from '../types/post'
+
+export const PAGE_SIZE = 5
+
+export async function getPosts() {
+  const res = await fetch('https://6144e843411c860017d256f0.mockapi.io/api/v1/posts')
+
+  if (!res.ok) {
+    throw new Error('Error fetching list of posts')
+  }
+
+  const posts: PostT[] = await res.json()
+  posts.sort((a,b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime())  // Sort by most recently created
+
+  const pageCount = Math.floor(posts.length / PAGE_SIZE) + (posts.length % PAGE_SIZE === 0 ? 0 : 1)
+
+  return { posts, pageCount }
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,24 +6,8 @@ import { ArrowBackIcon, ArrowForwardIcon } from '@chakra-ui/icons'
 import { Button, Center, Flex, Spacer, VStack } from '@chakra-ui/react'
 
 import Post from '../components/Post'
+import { PAGE_SIZE, getPosts } from '../lib/posts'
 import type PostT from '../types/post'
-
-const PAGE_SIZE = 5
-
-async function getPosts() {
-  const res = await fetch('https://6144e843411c860017d256f0.mockapi.io/api/v1/posts')
-
-  if (!res.ok) {
-    throw new Error('Error fetching list of posts')
-  }
-
-  const posts: PostT[] = await res.json()
-  posts.sort((a,b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime())  // Sort by most recently created
-
-  const pageCount = Math.floor(posts.length / PAGE_SIZE) + (posts.length % 5 === 0 ? 0 : 1)
-
-  return { posts, pageCount }
-}
 
 type Query = {
   posts: PostT[]
