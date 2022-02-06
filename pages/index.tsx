715514d28d3ba348e8b4522bdff37454056f248a
Jacob Prud'homme <jacobprudhomme@users.noreply.github.com>
2022-02-06 00:55:47 -0500

    Created hacky pagination

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,14 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import { useQuery } from 'react-query';
-import { Center, VStack } from '@chakra-ui/react'
+import { useState } from 'react'
+import { useQuery } from 'react-query'
+import { ArrowBackIcon, ArrowForwardIcon } from '@chakra-ui/icons'
+import { Button, Center, Flex, Spacer, VStack } from '@chakra-ui/react'
 
 import Post from '../components/Post'
 import type PostT from '../types/post'
+
+const PAGE_SIZE = 5
 
 async function getPosts() {
   const res = await fetch('https://6144e843411c860017d256f0.mockapi.io/api/v1/posts')
@@ -16,11 +20,20 @@ async function getPosts() {
   const posts: PostT[] = await res.json()
   posts.sort((a,b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime())  // Sort by most recently created
 
-  return posts
+  const pageCount = Math.floor(posts.length / PAGE_SIZE) + (posts.length % 5 === 0 ? 0 : 1)
+
+  return { posts, pageCount }
+}
+
+type Query = {
+  posts: PostT[]
+  pageCount: number
 }
 
 const Home: NextPage = () => {
-  const { isLoading, isError, data: posts, error } = useQuery<PostT[], Error>('posts', getPosts)
+  const [page, setPage] = useState(1)
+  const { isLoading, isError, data, error } = useQuery<Query, Error>('posts', getPosts)
+  const { posts, pageCount } = data || { posts: [], pageCount: 1 }
 
   return (
     <>
@@ -43,8 +56,26 @@ const Home: NextPage = () => {
             ? <p>Loading...</p>
             : isError
               ? <p>There was an error: {error?.message}</p>
-              : posts?.map(post => <Post key={post.id} post={post} />)
+              : Array.from({ length: PAGE_SIZE }, (_, i) => (page - 1) * PAGE_SIZE + i)
+                  .map(i => i < posts.length && <Post key={posts[i].id} post={posts[i]} />)
           }
+          <Flex>
+            <Button
+              leftIcon={<ArrowBackIcon />}
+              onClick={() => { setPage(page - 1); window.scrollTo(0, 0) }}
+              disabled={page === 1}
+            >
+              Prev Page
+            </Button>
+            <Spacer />
+            <Button
+              rightIcon={<ArrowForwardIcon />}
+              onClick={() => { setPage(page + 1); window.scrollTo(0, 0) }}
+              disabled={page === pageCount}
+            >
+              Next Page
+            </Button>
+          </Flex>
         </VStack>
       </Center>
     </>
