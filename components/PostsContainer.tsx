2a5649056d7e5f28e9e2fa94864d05bd21441e83
Jacob Prud'homme <jacobprudhomme@users.noreply.github.com>
2022-02-07 00:18:01 -0500

    Separated out container for posts

---
+++ components/PostsContainer.tsx
@@ -0,0 +1,77 @@
+import { useState } from 'react'
+import { useQuery } from 'react-query'
+import { ArrowBackIcon, ArrowForwardIcon } from '@chakra-ui/icons'
+import { Button, Flex, Heading, Spacer, Text, VStack } from '@chakra-ui/react'
+
+import Post from './Post'
+import { PAGE_SIZE, getPosts } from '../lib/posts'
+import type { PostT } from '../types/commentPost'
+
+type Query = {
+  posts: PostT[]
+  pageCount: number
+}
+
+const PostsContainer: React.VFC = () => {
+  const { isLoading, isError, data, error } = useQuery<Query, Error>('posts', getPosts)
+  const { posts, pageCount } = data || { posts: [], pageCount: 1 }
+  const [page, setPage] = useState(1)
+
+  if (isLoading) {
+    return <Heading color='teal.500'>Loading...</Heading>
+  } else if (isError) {
+    return (
+      <VStack>
+        <Heading color='teal.500'>There was an error</Heading>
+        <Text>{error?.message}</Text>
+      </VStack>
+    )
+  } else {
+    const postsToShow = Array
+      .from(
+        { length: PAGE_SIZE },
+        (_, i) => (page - 1) * PAGE_SIZE + i
+      )
+      .map(i =>
+        i < posts.length &&
+          <Post key={posts[i].id} post={posts[i]} />
+      )
+
+    return (
+      <VStack
+        w={{
+          base: '90%',
+          md: '80%',
+          xl: '50%',
+        }}
+        spacing={8}
+        align='stretch'
+      >
+        {postsToShow}
+        <Flex>
+          <Button
+            leftIcon={<ArrowBackIcon />}
+            onClick={() => { setPage(page - 1); window.scrollTo(0, 0) }}
+            bgColor='gray.50'
+            disabled={page === 1}
+            _hover={{ background: 'gray.300' }}
+          >
+            Prev Page
+          </Button>
+          <Spacer />
+          <Button
+            rightIcon={<ArrowForwardIcon />}
+            onClick={() => { setPage(page + 1); window.scrollTo(0, 0) }}
+            bgColor='gray.50'
+            disabled={page === pageCount}
+            _hover={{ background: 'gray.300' }}
+          >
+            Next Page
+          </Button>
+        </Flex>
+      </VStack>
+    )
+  }
+}
+
+export default PostsContainer
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,9 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import { useState } from 'react'
-import { useQuery } from 'react-query'
-import { ArrowBackIcon, ArrowForwardIcon } from '@chakra-ui/icons'
-import { Button, Center, Flex, Heading, Spacer, Text, VStack } from '@chakra-ui/react'
-
-import Post from '../components/Post'
-import { PAGE_SIZE, getPosts } from '../lib/posts'
-import type { PostT } from '../types/commentPost'
-
-type Query = {
-  posts: PostT[]
-  pageCount: number
-}
+import { Center } from '@chakra-ui/react'
+import PostsContainer from '../components/PostsContainer'
 
 const Home: NextPage = () => {
-  const [page, setPage] = useState(1)
-  const { isLoading, isError, data, error } = useQuery<Query, Error>('posts', getPosts)
-  const { posts, pageCount } = data || { posts: [], pageCount: 1 }
 
   return (
     <>
@@ -35,53 +21,7 @@ const Home: NextPage = () => {
         }}
         bgColor='gray.50'
       >
-        {isLoading
-          ? <Heading color='teal.500'>Loading...</Heading>
-          : isError
-            ? (
-                <VStack>
-                  <Heading color='teal.500'>There was an error</Heading>
-                  <Text>{error?.message}</Text>
-                </VStack>
-              )
-            : (
-                <VStack
-                  w={{
-                    base: '90%',
-                    md: '80%',
-                    xl: '50%',
-                  }}
-                  spacing={8}
-                  align='stretch'
-                >
-                  {
-                    Array.from({ length: PAGE_SIZE }, (_, i) => (page - 1) * PAGE_SIZE + i)
-                        .map(i => i < posts.length && <Post key={posts[i].id} post={posts[i]} />)
-                  }
-                  <Flex>
-                    <Button
-                      leftIcon={<ArrowBackIcon />}
-                      onClick={() => { setPage(page - 1); window.scrollTo(0, 0) }}
-                      bgColor='gray.50'
-                      disabled={page === 1}
-                      _hover={{ background: 'gray.300' }}
-                    >
-                      Prev Page
-                    </Button>
-                    <Spacer />
-                    <Button
-                      rightIcon={<ArrowForwardIcon />}
-                      onClick={() => { setPage(page + 1); window.scrollTo(0, 0) }}
-                      bgColor='gray.50'
-                      disabled={page === pageCount}
-                      _hover={{ background: 'gray.300' }}
-                    >
-                      Next Page
-                    </Button>
-                  </Flex>
-                </VStack>
-              )
-        }
+        <PostsContainer />
       </Center>
     </>
   )
