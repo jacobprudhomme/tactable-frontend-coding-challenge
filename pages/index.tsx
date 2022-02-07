8db8c794d59a8f6038b955e6d96a0b7ddda223af
Jacob Prud'homme <jacobprudhomme@users.noreply.github.com>
2022-02-07 00:01:57 -0500

    Fixed index page style

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Head from 'next/head'
 import { useState } from 'react'
 import { useQuery } from 'react-query'
 import { ArrowBackIcon, ArrowForwardIcon } from '@chakra-ui/icons'
-import { Button, Center, Flex, Spacer, VStack } from '@chakra-ui/react'
+import { Button, Center, Flex, Heading, Spacer, Text, VStack } from '@chakra-ui/react'
 
 import Post from '../components/Post'
 import { PAGE_SIZE, getPosts } from '../lib/posts'
@@ -26,41 +26,62 @@ const Home: NextPage = () => {
         <link rel='icon' href='/favicon.ico' />
       </Head>
 
-      <Center minH='100vh' w='100vw' py={16}>
-        <VStack
-          w='50%'
-          p={6}
-          borderWidth='1px'
-          borderRadius='md'
-          borderColor='gray.100'
-          align='stretch'
-          spacing={6}
-        >
-          {isLoading
-            ? <p>Loading...</p>
-            : isError
-              ? <p>There was an error: {error?.message}</p>
-              : Array.from({ length: PAGE_SIZE }, (_, i) => (page - 1) * PAGE_SIZE + i)
-                  .map(i => i < posts.length && <Post key={posts[i].id} post={posts[i]} />)
-          }
-          <Flex>
-            <Button
-              leftIcon={<ArrowBackIcon />}
-              onClick={() => { setPage(page - 1); window.scrollTo(0, 0) }}
-              disabled={page === 1}
-            >
-              Prev Page
-            </Button>
-            <Spacer />
-            <Button
-              rightIcon={<ArrowForwardIcon />}
-              onClick={() => { setPage(page + 1); window.scrollTo(0, 0) }}
-              disabled={page === pageCount}
-            >
-              Next Page
-            </Button>
-          </Flex>
-        </VStack>
+      <Center
+        minH='100vh'
+        w='100vw'
+        py={{
+          base: 4,
+          md: 16,
+        }}
+        bgColor='gray.50'
+      >
+        {isLoading
+          ? <Heading color='teal.500'>Loading...</Heading>
+          : isError
+            ? (
+                <VStack>
+                  <Heading color='teal.500'>There was an error</Heading>
+                  <Text>{error?.message}</Text>
+                </VStack>
+              )
+            : (
+                <VStack
+                  w={{
+                    base: '90%',
+                    md: '80%',
+                    xl: '50%',
+                  }}
+                  spacing={8}
+                  align='stretch'
+                >
+                  {
+                    Array.from({ length: PAGE_SIZE }, (_, i) => (page - 1) * PAGE_SIZE + i)
+                        .map(i => i < posts.length && <Post key={posts[i].id} post={posts[i]} />)
+                  }
+                  <Flex>
+                    <Button
+                      leftIcon={<ArrowBackIcon />}
+                      onClick={() => { setPage(page - 1); window.scrollTo(0, 0) }}
+                      bgColor='gray.50'
+                      disabled={page === 1}
+                      _hover={{ background: 'gray.300' }}
+                    >
+                      Prev Page
+                    </Button>
+                    <Spacer />
+                    <Button
+                      rightIcon={<ArrowForwardIcon />}
+                      onClick={() => { setPage(page + 1); window.scrollTo(0, 0) }}
+                      bgColor='gray.50'
+                      disabled={page === pageCount}
+                      _hover={{ background: 'gray.300' }}
+                    >
+                      Next Page
+                    </Button>
+                  </Flex>
+                </VStack>
+              )
+        }
       </Center>
     </>
   )
